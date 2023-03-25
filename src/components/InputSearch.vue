1314f34d53c943d23f5919247f102d7ecd2294cd
ngocduy1226 <lamngocduy2001@gmail.com>
2023-03-24 16:32:46 -0800

    Cai dat trang loi 404

--- src/components/InputSearch.vue
+++ src/components/InputSearch.vue
@@ -1,5 +1,4 @@
 <script>
-
 export default {
     props: {
          modelValue: { type: String, default: "" },
@@ -9,16 +8,12 @@ export default {
         updateModelValue(e) {
             this.$emit("update:modelValue", e.target.value);
         },
-        //Với việc định nghĩa thuộc tính modelValue, liên kết value của input với modelValue và phát sinh sự
-        // kiện update:modelValue khi value của input thay đổi cho phép ta sử dụng v-model tạo liên kết hai
-        // chiều với InputSearch, ví dụ: <InputSearch v-model="..." />
         submit() {
             this.$emit("submit");
         },
     },
 };
 </script>
-
 <template>
     <div class="input-group">
         <input
--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-// import './assets/main.css'
 
 createApp(App).mount('#app');
 
